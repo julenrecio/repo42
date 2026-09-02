3e2062b8a4ac6b252d2ed6842540ddf9154a7df0
Julen Recio <julenreciotocado@gmail.com>
2026-09-02 18:03:33 +0200

    Current progress on module 09

---
+++ Cursus42/python_module_09/ex0/space_station.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from pydantic import BaseModel, Field, ValidationError
+from datetime import datetime
+
+
+class SpaceStation(BaseModel):
+    station_id: str = Field(min_length=3, max_length=10)
+    name: str = Field(min_length=1, max_length=50)
+    crew_size: int = Field(le=20)
+    power_level: float = Field(ge=0.0, le=100.0)
+    oxygen_level: float = Field(ge=0.0, le=100.0)
+    last_maintenance: datetime
+    is_operational: bool = Field(default=True)
+    notes: str | None = Field(default=None, max_length=200)
+
+
+if __name__ == "__main__":
+    iss001: SpaceStation = SpaceStation(station_id="ISS001",
+                                        name="International Space Station",
+                                        crew_size=6, power_level=85.5,
+                                        oxygen_level=92.3,
+                                        last_maintenance=datetime.now(),
+                                        is_operational=True)
+    print("Space Station Data Validation")
+    print("========================================")
+    print("Valid station created:")
+    print("ID:", iss001.station_id)
+    print("Name:", iss001.name)
+    print("Crew:", iss001.crew_size, "people")
+    print("Power: ", iss001.power_level, "%", sep="")
+    print("Oxygen: ", iss001.oxygen_level, "%", sep="")
+
+    if (iss001.is_operational):
+        print("Status: Operational")
+    else:
+        print("Status: Not operational")
+    if (iss001.notes):
+        print("Notes:", iss001.notes)
+
+    print("\n========================================")
+    print("Expected validation error:")
+
+    try:
+        iss002: SpaceStation = SpaceStation(station_id="ISS001",
+                                            name="International Space Station",
+                                            crew_size=42, power_level=85.5,
+                                            oxygen_level=92.3,
+                                            last_maintenance=datetime.now(),
+                                            is_operational=True)
+    except ValidationError as e:
+        print(e.errors()[0].get("msg"))
---
+++ Cursus42/python_module_09/ex1/alien_contact.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from pydantic import BaseModel, Field
+from datetime import datetime
+from enum import Enum
+
+class ContactType(Enum):
+    radio = 1
+    visual = 2
+    physical = 3
+    telepathic = 4
+
+class AlienContact(BaseModel):
+    contact_id: str = Field(min_length=5, max_length=15)
+    timestamp: datetime = Field()
+    location: str = Field(min_length=3, max_length=100)
+    contact_type: ContactType = Field()
+    signal_strength: float = Field(ge=0.0, le=10.0)
+    duration_minutes: int = Field(ge=1, le=1440)
+    witness_count: int = Field(ge=1, le=100)
+    message_received: str | None = Field(default=None, max_length=500)
+    is_verified: bool = Field(default=False)
